d6b0bc3631bd806a64758bd413aa09c178a9a1e9
Nick Barth <barth.nicholas@gmail.com>
2016-07-19 22:12:26 +0200

    gutting boilerplate

--- config/env/default.js
+++ config/env/default.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
+    title: 'psFriend',
+    description: 'Send something nice.  Make smiles.',
     keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
@@ -36,6 +36,8 @@ module.exports = {
     p3p: 'ABCDEF',
     xssProtection: true
   },
+
+	// favicon and logo go here
   logo: 'modules/core/client/img/brand/logo.png',
   favicon: 'modules/core/client/img/brand/favicon.ico',
   uploads: {
