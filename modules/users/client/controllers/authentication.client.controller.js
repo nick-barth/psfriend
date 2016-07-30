e5527b3107ca76409a3ed09aeb2c63975849fdf3
Nick Barth <barth.nicholas@gmail.com>
2016-07-30 18:05:32 +0200

    formatting mostly

--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -56,12 +56,12 @@ function AuthenticationController ($scope, $state, $http, $location, $window, Au
 		$http.post('/api/auth/signin', vm.credentials).success(function (response) {
 							// If successful we assign the response to the global user model
 							vm.authentication.user = response;
-
 							// And redirect to the previous or home page
 							$state.go($state.previous.state.name || 'home', $state.previous.params);
 						}).error(function (response) {
-							vm.error = response.message;
-						});
+								console.log(response);
+								vm.error = response.message;
+							});
 	}
 
 	// OAuth provider request
