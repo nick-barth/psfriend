ec1614690a5c162b87eb484974988c743dde23da
Nick Barth <barth.nicholas@gmail.com>
2016-08-06 16:58:29 +0200

    backlink baby

--- modules/product/client/controllers/payment.client.controller.js
+++ modules/product/client/controllers/payment.client.controller.js
@@ -10,6 +10,12 @@ PaymentController.$inject = ['$scope', '$state', '$http', 'ProductService'];
 function PaymentController ($scope, $state, $http, ProductService) {
 	$scope.ProductService = ProductService.product;
 
+	if (productService.user) {
+		$scope.backLink = '/onboard/address';
+	} else {
+		$scope.backLink = '/onboard/signup';
+	};
+
 	$scope.submit = function () {
 		$http.post('/api/product/add', $scope.ProductService)
 			.success(function (response) {
