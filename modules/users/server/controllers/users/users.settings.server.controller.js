b3f93cb9bf041ec29111b0b404c17977548ef9d8
Nick Barth <barth.nicholas@gmail.com>
2016-08-15 18:29:43 +0200

    got removal from stripe approval!

--- modules/users/server/controllers/users/users.settings.server.controller.js
+++ modules/users/server/controllers/users/users.settings.server.controller.js
@@ -34,30 +34,40 @@ exports.settings = function (req, res) {
 };
 
 exports.cancel = function (req, res) {
-	Product.findById(req.body.id, function (err, doc) {
-	})
-	.remove(function (err)
-	{
-		if (!err) {
-			Product.find({ 'user.email': req.user.email }, function (err, docs) {
-				if (err) {
-					return res.status(400).send({
-						message: 'No current active subscriptions'
-					});
-				} else {
-					var subs = _.map(docs, function (doc) {
-						let newObj = {
-							card: doc.card,
-							address: doc.address,
-							id: doc._id
-						};
-						return newObj;
-					});
-					return res.status(200).send({
-						subs: subs
+	console.log(req.body.id);
+	Product.find({ '_id': req.body.id }, function (err, doc) {
+		stripe.subscriptions.del(
+			doc[0].sub_id,
+			function (err, confirmation) {
+				//TODO: Add Error Handling
+				if (!err) {
+					Product.find({ '_id': req.body.id }, function (err, doc) {
+					}).remove(function (err)
+					{
+						if (!err) {
+							Product.find({ 'user.email': req.user.email }, function (err, docs) {
+								if (err) {
+									return res.status(400).send({
+										message: 'No current active subscriptions'
+									});
+								} else {
+									var subs = _.map(docs, function (doc) {
+										let newObj = {
+											card: doc.card,
+											address: doc.address,
+											id: doc._id
+										};
+										return newObj;
+									});
+									return res.status(200).send({
+										subs: subs
+									});
+								}
+							});
+						}
 					});
 				}
-			});
-		}
+			}
+		);
 	});
 };
