b9403c21c420325738a03e1649536870f94fa7e5
Nick Barth <barth.nicholas@gmail.com>
2016-08-20 17:28:30 +0200

    removed menu service

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -1,25 +1,12 @@
 (function () {
-  'use strict';
+'use strict';
 
-  angular
-    .module('core')
-    .controller('HeaderController', HeaderController);
+angular
+		.module('core')
+		.controller('HeaderController', HeaderController);
 
-  HeaderController.$inject = ['$scope', '$state', 'Authentication', 'menuService'];
+HeaderController.$inject = ['$scope', '$state', 'Authentication'];
 
-  function HeaderController($scope, $state, Authentication, menuService) {
-    var vm = this;
-
-    vm.accountMenu = menuService.getMenu('account').items[0];
-    vm.authentication = Authentication;
-    vm.isCollapsed = false;
-    vm.menu = menuService.getMenu('topbar');
-
-    $scope.$on('$stateChangeSuccess', stateChangeSuccess);
-
-    function stateChangeSuccess() {
-      // Collapsing the menu after navigation
-      vm.isCollapsed = false;
-    }
-  }
+function HeaderController ($scope, $state, Authentication) {
+}
 }());
