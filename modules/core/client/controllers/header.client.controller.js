714ae27acad7ba2d741a877a1176dce648c3ce28
Nick Barth <barth.nicholas@gmail.com>
2016-08-22 09:33:35 +0200

    trying to get this scroll bar dynamic

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -5,8 +5,22 @@ angular
 		.module('core')
 		.controller('HeaderController', HeaderController);
 
-HeaderController.$inject = ['$scope', '$state', 'Authentication'];
+HeaderController.$inject = ['$scope', '$state', '$document', '$window', '$timeout', 'Authentication'];
 
-function HeaderController ($scope, $state, Authentication) {
+function HeaderController ($scope, $state, $document, $window, $timeout, Authentication) {
+	//bad and annoying hack to determine states name, ui router :(
+	$timeout(function () {
+		if ($state.current.name == 'home') {
+			$scope.fixed = true;
+			$document.on('scroll', function () {
+				if ($window.scrollY > 10) {
+					$scope.scrolled = true;
+					console.log($scope.scrolled);
+				} else {
+					$scope.scrolled = false;
+				}
+			});
+		}
+	}, 100);
 }
 }());
