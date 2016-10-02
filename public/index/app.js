3c09b087417bfd9d5dfd9b5c6373b44eea2f604d
Nick Barth <barth.nicholas@gmail.com>
2016-10-02 16:51:57 +0200

    added easing to the site

--- public/index/app.js
+++ public/index/app.js
@@ -1,4 +1,14 @@
 jQuery(function ($) {
+
+	$.extend(jQuery.easing,{easeInOutQuad:function(t) {return t<.5 ? 2*t*t : -1+(4-2*t)*t }})
+
+	$('.home__anchorbar-anchor').click(function () {
+		$('html, body').animate({
+			scrollTop: $('.' + $(this).data('anchor')).offset().top
+		}, 1000, 'easeInOutQuad');
+		return false;
+	});
+
 	var $anchorbar = $('.home__anchorbar');
 	var offsetTop = $anchorbar.offset().top;
 	function fixDiv () {
